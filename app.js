0c8da1bf9eb8ce21cbfaefd16f33454396102f1b
Alex <alextutovan@tutanota.com>
2020-01-29 16:11:43 +0100

    Website working, version 1.0

--- app.js
+++ app.js
@@ -1,9 +1,45 @@
 const toggleMenu = document.querySelector(".menu");
-const toggleMenuButton = document.querySelector(".menu-toggle");
-const closeMenuButton = document.querySelector(".menu-close");
+const toggleMenuButton = document.querySelector(".menu__toggle");
+const closeMenuButton = document.querySelector(".menu__toggle--close");
+const modalButton = document.querySelector(".modal__button");
 const body = document.body;
 
 toggleMenuButton.addEventListener("click", toggleMenuHandler);
+document.addEventListener(
+  "click",
+  e => {
+    e.preventDefault();
+
+    if (
+      e.target.className === "navbar__list__item__link" ||
+      e.target.className === "menu__list__item__link" ||
+      e.target.className === "social__media__link" ||
+      e.target.className === "legal__link"
+    ) {
+      const modal = document.createElement("section");
+      modal.className = "modal";
+      modal.innerHTML = `<article class="modal__box">
+      <h1>Hello visitor</h1>
+      <p class="modal__message">
+        Links do not work yet on this website because it is intended to
+        resemble just a landing page
+      </p>
+      <button class="modal__button">Ok, get me back to the page</button>
+     </article>`;
+      body.appendChild(modal);
+      body.style.overflow = "hidden";
+    }
+  },
+  false
+);
+
+body.addEventListener("click", e => {
+  e.preventDefault();
+  if (e.target.className === "modal__button") {
+    body.removeChild(document.querySelector(".modal"));
+    body.style.overflow = "scroll";
+  }
+});
 
 // Function for opening/closing the menu on mobile/tablet
 function toggleMenuHandler(e) {
@@ -17,7 +53,7 @@ function toggleMenuHandler(e) {
     body.classList.remove("overflow-hidden");
   }
 
-  toggleMenuButton.classList.contains("close-menu-icon")
-    ? toggleMenuButton.classList.remove("close-menu-icon")
-    : toggleMenuButton.classList.add("close-menu-icon");
+  toggleMenuButton.classList.contains("menu__toggle--close")
+    ? toggleMenuButton.classList.remove("menu__toggle--close")
+    : toggleMenuButton.classList.add("menu__toggle--close");
 }
